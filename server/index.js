514a24fd73f8f1c5d644693981794a681b21912d
naseralmuhana <naseralmuhana0@gmail.com>
2022-12-20 02:06:09 +0300

    install morgan

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import colors from "colors"
+import morgan from "morgan"
 
 // To be able to use env variables
 dotenv.config()
@@ -10,6 +11,10 @@ const app = express()
 app.get("/", (req, res) => {
   res.send("API IS RUNNING....")
 })
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"))
+}
 
 const PORT = process.env.PORT || 5000
 const MODE = process.env.NODE_ENV || "development"
