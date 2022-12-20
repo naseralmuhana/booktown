f18d996001c75eb9a7066b39e5f6c5ee774fcc33
naseralmuhana <naseralmuhana0@gmail.com>
2022-12-20 03:30:10 +0300

    book model & controllers & routes

---
+++ server/models/review.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import slug from "mongoose-slug-generator"
+
+mongoose.plugin(slug)
+
+export const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    slug: { type: String, slug: "name" },
+  },
+  { timestamps: true }
+)
